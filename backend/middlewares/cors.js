d9e0af0482eed1a949e2c5a9ce193635b4f82057
Julia Perfilova <perfilovajulia@yandex.ru>
2022-09-19 23:59:31 +0300

    feat: add cors preflight

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,31 +4,19 @@ const allowedCors = [
   'localhost:3000',
 ];
 
+const DEFAULT_ALLOWED_METHODS = 'GET,PUT,PATCH,POST,DELETE';
+
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
   next();
 };
-
-/* const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-// Если это предварительный запрос, добавляем нужные заголовки
-if (method === 'OPTIONS') {
-  // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-  res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-}
-
-// сохраняем список заголовков исходного запроса
-const requestHeaders = req.headers['access-control-request-headers'];
-if (method === 'OPTIONS') {
-
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-} */
