34dcc8dfcd509ae0c65d6a06ab7f4b9ff2ac5baa
Julia Perfilova <perfilovajulia@yandex.ru>
2022-09-26 18:44:39 +0300

    Remove comment

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,7 +10,6 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    // res.header('Access-Control-Allow-Credentials', true);
   }
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
